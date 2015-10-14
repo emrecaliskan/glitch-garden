d6dc30dbf010ea5a975ab1f54fee322ef6730857
Emre Caliskan <emre.caliskan@outlook.com>
2015-10-13 23:48:41 -0700

    Initial commit.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter = 0;	
+	
+	void Start(){
+		if(autoLoadNextLevelAfter == 0){
+			Debug.Log ("Level auto load disabled");
+		} else {
+			Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+
+	public void LoadLevel(string name){
+		Debug.Log ("New Level Load: " + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest(){
+		Debug.Log ("Quit Requested");
+		Application.Quit(); 
+	}	
+	
+	public void LoadNextLevel(){
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+
+	private AudioSource audioSource; 
+	
+	void Awake(){
+		DontDestroyOnLoad(gameObject);
+		Debug.Log ("Dont destroy on load: " + name);
+	}
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+
+	}
+	
+	void OnLevelWasLoaded(int level){
+		Debug.Log ("Started");
+		Debug.Log ("Level: " + level);
+		AudioClip audioClip = levelMusicChangeArray[level];
+		Debug.Log ("Playing clip: " + audioClip);
+		
+		if (audioClip){	// If there is an audio clip attached
+			audioSource.clip = audioClip;
+			audioSource.loop = false;
+			audioSource.Play();
+		}
+		
+		
+	}
+}
