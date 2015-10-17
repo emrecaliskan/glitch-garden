0a95684257ade411d4b8ab7c911fbc2c28a5195d
Emre Caliskan <emre.caliskan@outlook.com>
2015-10-17 12:01:43 -0700

    Added dealing damage functionality and health component to attackers and defenders.

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -5,18 +5,23 @@ public class Attacker : MonoBehaviour {
 
 	private float currentSpeed;
 	private GameObject currentTarget;
+	private Animator animator;
 
 	// Use this for initialization
 	void Start () {
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
+		if (!currentTarget){
+			animator.SetBool("isAttacking", false);
+		}
 	}
 	
 	void OnTriggerEnter2D(){
-	
+		
 		Debug.Log(name + " trigger enter.");
 	}
 	
@@ -27,10 +32,16 @@ public class Attacker : MonoBehaviour {
 	// Called from the animator at time of actual attack
 	public void StrikeCurrentTarget(float damage){
 		Debug.Log ("Doing Damage:" + damage);
+		if(currentTarget){
+			Health health = currentTarget.GetComponent<Health>();
+			if (health) {
+				health.DealDamage(damage);
+			}
+		}
+		
 	}
 	
 	public void Attack(GameObject obj){
 		currentTarget = obj;
-		
 	}
 }
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	public float health = 100;
+	
+	public void DealDamage(float damage){
+		health -= damage;
+		if(health <=0){
+			//Optionally trigger die animation
+			DestroyObject();
+		}
+	}
+	
+	// Create seperate destroy object function to call as animator event.
+	public void DestroyObject(){
+		DestroyObject(gameObject);
+	}
+}
